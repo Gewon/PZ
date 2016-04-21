4969f7192a8de4dc37b3604d9b9d69c2ad96ff76
Paweł <pawel.wloch94@gmail.com>
2016-04-21 20:46:42 +0200

    Domyslna apka

---
+++ ProjektZespolowy/ProjektZespolowy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjektZespolowy.Startup))]
+namespace ProjektZespolowy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
